b6556ec13922f7e2afc82315205dee7063f2d3b6
Tyler <trc113@txstate.edu>
2020-01-29 17:39:11 -0600

    added timestep to app

--- app.js
+++ app.js
@@ -24,6 +24,13 @@ io.on('connection', function(socket) {
     markers[thisMarkerID] = marker;
     sockets[thisMarkerID] = socket;
 
+    setInterval(() => {
+        var rando = Math.floor(Math.random() * 101)
+        socket.emit('MyData', rando)
+    }, 100);
+
+    
+
     //Tell the client that this our id for the server
     
     //if the client that was connected is not in the list of marker names we can disconnect it
